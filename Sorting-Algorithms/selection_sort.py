4b2ef001630084f381e7bae785be62e1f253f667
Manish Sharma <69568590+gargmani312@users.noreply.github.com>
2024-10-09 11:20:43 +0530

    selection sort

---
+++ Sorting-Algorithms/selection_sort.py
@@ -0,0 +1,18 @@
+def select_sort(arr):
+    '''
+    params: list of integers
+    returns: sorted list of integers
+    '''    
+    len_of_arr = len(arr)
+    for i in range(len_of_arr):
+        min_index = i
+        for j in range(i+1, len_of_arr):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [64, 25, 12, 22, 11]
+    print(select_sort(arr))
